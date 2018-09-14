bdb2bd298eceb1a0c02f8a5b757247fc2a19b85c
9Strike <36662302+9Strike@users.noreply.github.com>
2018-09-14 11:48:40 +0200

    Update Versuch26.py

--- PAP1/Versuch26.py
+++ PAP1/Versuch26.py
@@ -14,7 +14,7 @@ M_CO2 = 44e-3
 c_CO2 = m.sqrt(k_CO2 * R * T0 / M_CO2)
 
 h0_xy = [3.5e-2, 3.5e-2, 3.5e-2, 3.5e-2, 3.5e-2, 3.5e-2, 3.5e-2, 3.4e-2]
-h0_xy = ms.mean_value(h0_xy)
+h0_xy = mean_value(h0_xy)
 h0_yt = 3.5e-2
 d_h0 = m.sqrt(2/7) * 0.1e-2
 c_xy = h0_xy * 10e3
@@ -22,12 +22,16 @@ c_yt = h0_yt * 10e3
 d_c = d_h0 * 10e3
 
 c0_xy = c_xy * m.sqrt(T0 / T)
-d_c0_xy = m.sqrt(T0 / T + (d_c**2 + 0.25 * (c_xy * d_T / T)**2))
+d_c0_xy = m.sqrt(T0 / T * (d_c**2 + 0.25 * (c_xy * d_T / T)**2))
 sigma_xy = abs(c_Luft - c0_xy) / d_c0_xy
 c0_yt = c_yt * m.sqrt(T0 / T)
-d_c0_yt = m.sqrt(T0 / T + (d_c**2 + 0.25 * (c_yt * d_T / T)**2))
+d_c0_yt = m.sqrt(T0 / T * (d_c**2 + 0.25 * (c_yt * d_T / T)**2))
 sigma_yt = abs(c_Luft - c0_yt) / d_c0_yt
 
+print()
+print(h0_xy)
+print(h0_yt)
+print(d_h0)
 print()
 print(c_Luft)
 print(c_CO2)
