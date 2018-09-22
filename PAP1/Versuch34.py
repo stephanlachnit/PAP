bde405d4330475cc71629c1b0747e1b9101338c1
9Strike <36662302+9Strike@users.noreply.github.com>
2018-09-22 22:38:08 +0200

    Add files via upload

--- PAP1/Versuch34.py
+++ PAP1/Versuch34.py
@@ -6,9 +6,9 @@ b0 = 42
 db0 = 3
 b = 30
 db = 3
+l = [1.5, 3.0, 6.0, 12.0, 24.0]
 
 il = [[63280.29, 63070.40, 63058.95, 62993.61, 63105.31],
-      [46607.18, 46647.68, 46587.64, 46623.69, 46628.55],
       [46607.18, 46647.68, 46587.64, 46623.69, 46628.55],
       [25813.76, 25864.76, 25832.21, 25804.32, 25801.63],
       [5206.06, 5182.66, 5183.84, 5176.01, 5202.05],
@@ -19,16 +19,35 @@ il = [ms.mean_value(il[i]) for i in range(len(il))]
 il[4] *= (b * b) / (b0 * b0)
 dil[4] = m.sqrt((4 * b / b0**2 * il[4] * db)**2 + (b**2 / b0**3 * il[4] * db0)**2 + (b**2 / b0**2 * dil[4])**2)
 
+for i in range(len(l)):
+  ms.pv("l" + str(i), l[i])
+print()
 for i in range(len(il)):
   ms.pve("il" + str(i), il[i], dil[i])
 
 # Messung mit variaabler Konzentration
-v0 = 21.0
-v1 = 1.4
+v = [21.0, 1.4, 1.6, 4.0, 14.0]
+ct = 1.0e-3
 
 ik = [[62905.49, 62315.77, 62290.07, 62279.29, 62243.04],
       [42832.73, 42892.47, 43043.32, 43101.21, 43228.01],
       [27760.81, 27836.17, 27755.93, 27715.35, 27603.68],
       [8208.01, 8366.15, 8491.14, 8561.95, 8646.58],
       [1487.15, 1458.05, 1564.75, 1531.03, 1604.25]]
+dik = [ms.std_dev_m(ik[i]) for i in range(len(ik))]
 ik = [ms.mean_value(ik[i]) for i in range(len(ik))]
+
+c = []
+vsum = 0
+for i in range(len(ik)):
+  c.append(ct * vsum / (vsum + v[0]))
+  if i < len(ik) - 1:
+    vsum += v[i + 1]
+
+print()
+print()
+for i in range(len(c)):
+  ms.pv("c" + str(i), c[i])
+print()
+for i in range(len(ik)):
+  ms.pve("ik" + str(i), ik[i], dik[i])
--- PAP1/measure.py
+++ PAP1/measure.py
@@ -1,4 +1,4 @@
-import math
+import math as m
 
 def mean_value(x):
   s = 0
@@ -10,48 +10,58 @@ def std_dev_e(x):
   qs = 0
   for i in range(len(x)):
     qs += (x[i] - mean_value(x))**2
-  return math.sqrt(qs / (len(x) - 1))
+  return m.sqrt(qs / (len(x) - 1))
   
 def std_dev_m(x):
   qs = 0
   for i in range(len(x)):
     qs += (x[i] - mean_value(x))**2
-  return math.sqrt(qs / ((len(x) - 1) * len(x)))
+  return m.sqrt(qs / ((len(x) - 1) * len(x)))
 
 def reg_grad(x, y, dy):
- s0 = 0
- s1 = 0
- s2 = 0
- s3 = 0
- s4 = 0
- for i in range(len(x)):
-  s0 += 1 / (dy[i] * dy[i])
-  s1 += x[i] / (dy[i] * dy[i])
-  s2 += y[i] / (dy[i] * dy[i])
-  s3 += x[i] * x[i] / (dy[i] * dy[i])
-  s4 += x[i] * y[i] / (dy[i] * dy[i])
- eta = s0 * s3 - s1 * s1
- return (s0 * s4 - s1 * s2) / eta
+  s0 = 0
+  s1 = 0
+  s2 = 0
+  s3 = 0
+  s4 = 0
+  for i in range(len(x)):
+    s0 += 1 / (dy[i] * dy[i])
+    s1 += x[i] / (dy[i] * dy[i])
+    s2 += y[i] / (dy[i] * dy[i])
+    s3 += x[i] * x[i] / (dy[i] * dy[i])
+    s4 += x[i] * y[i] / (dy[i] * dy[i])
+  eta = s0 * s3 - s1 * s1
+  return (s0 * s4 - s1 * s2) / eta
 
 def reg_grad_err(x, y, dy):
- s0 = 0
- s1 = 0
- s2 = 0
- s3 = 0
- s4 = 0
- for i in range(len(x)):
-  s0 += 1 / (dy[i] * dy[i])
-  s1 += x[i] / (dy[i] * dy[i])
-  s2 += y[i] / (dy[i] * dy[i])
-  s3 += x[i] * x[i] / (dy[i] * dy[i])
-  s4 += x[i] * y[i] / (dy[i] * dy[i])
- eta = s0 * s3 - s1 * s1
- return math.sqrt(s0 / eta)
+  s0 = 0
+  s1 = 0
+  s2 = 0
+  s3 = 0
+  s4 = 0
+  for i in range(len(x)):
+    s0 += 1 / (dy[i] * dy[i])
+    s1 += x[i] / (dy[i] * dy[i])
+    s2 += y[i] / (dy[i] * dy[i])
+    s3 += x[i] * x[i] / (dy[i] * dy[i])
+    s4 += x[i] * y[i] / (dy[i] * dy[i])
+  eta = s0 * s3 - s1 * s1
+  return m.sqrt(s0 / eta)
 
-def pv(name, var):
- print()
- print(name + " = " + str(var))
+def pv(name, val):
+  print()
+  print(name + " = " + str(val))
 
-def pve(name, var, err):
- print()
- print(name + " = " + str(var) + " ± " + str(err))
+def pve(name, val, err):
+  print()
+  sigerr = float("{0:.1e}".format(err))
+  sigval = round(val, 1 - int(m.floor(m.log10(sigerr))))
+  print(name + " = " + str(sigval) + " ± " + str(sigerr))
+
+def ple(name, val, err):
+  print()
+  print(name + ":")
+  for i in range(len(val)):
+    sigerr = float("{0:.1e}".format(err[i]))
+    sigval = round(val[i], 1 - int(m.floor(m.log10(sigerr))))
+    print(str(sigval) + " ± " + str(sigerr))
