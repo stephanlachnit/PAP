aa17526455a88f6f4ae63e2840e2af1af55cea20
9Strike <36662302+9Strike@users.noreply.github.com>
2018-09-28 21:08:33 +0200

    Add files via upload

--- PAP1/Versuch42.py
+++ PAP1/Versuch42.py
@@ -16,13 +16,10 @@ Tw = 55.8
 dTw = 0.2
 Tl = 24.1
 dTl = 0.1
-Tm = 53.25
-dTm = 0.27
+Tm = 53.25 # 53.19
+dTm = 0.27 # 0.4
 W = mw * cw * (Tw - Tm) / (Tm - Tl)
 dW = m.sqrt((Tw - Tm)**2 * ((cw * dmw)**2 + (mw * dcw)**2) + (mw * cw)**2 * (dTw**2 + ( ((Tl - Tw) * dTm)**2 + ((Tw - Tm) * dTl)**2) / (Tm - Tl)**2)) / (Tm - Tl)
-
-W = 92.0
-dW = 17.0
 
 ms.pve("mw", mw, dmw)
 ms.pve("Wasserwert", W, dW)
@@ -41,39 +38,44 @@ Tw = [27.4, 24.7, 23.4]
 dTw = [0.2, 0.2, 0.2]
 Tp = [100 + 0.0276 * (p - p0) for i in range(len(mp))]
 dTp = [0.0276 * dp for i in range(len(mp))]
-
 Tm = [29.7, 29.5, 27.4]
 dTm = [0.1, 0.1, 0.1]
 
-cp = []
-dcp = []
-cpMol = []
-dcpMol = []
+cpH = []
+dcpH = []
+cpMolH = []
+dcpMolH = []
 for i in range(len(mp)):
   mw[i] -= mk
   dmw[i] = m.sqrt(dmw[i]**2 + dmk**2)
-  cp.append((mw[i] * cw + W) * (Tm[i] - Tw[i]) / (mp[i] * (Tp[i] - Tm[i])))
-  dcp.append(1.0 / (mp[i] * (Tp[i] - Tm[i])) * m.sqrt(
+  cpH.append((mw[i] * cw + W) * (Tm[i] - Tw[i]) / (mp[i] * (Tp[i] - Tm[i])))
+  dcpH.append(1.0 / (mp[i] * (Tp[i] - Tm[i])) * m.sqrt(
       (Tm[i] - Tw[i])**2 * ((cw * dmw[i])**2 + (mw[i] * dcw)**2 + dW**2
     + (mw[i] * cw + W)**2 * ((dmp[i] / mp[i])**2 + (dTp[i] / (Tp[i] - Tm[i]))**2))
     + (mw[i] * cw + W)**2 * (dTw[i]**2 + ((Tp[i] - Tw[i]) / (Tp[i]- Tm[i]) * dTm[i])**2)))
-  cpMol.append(Mp[i] * cp[i])
-  dcpMol.append(Mp[i] * dcp[i])
+  cpMolH.append(Mp[i] * cpH[i])
+  dcpMolH.append(Mp[i] * dcpH[i])
 
 sigma = [129.0, 900.0, 709.0]
 for i in range(len(sigma)):
-  sigma[i] = (sigma[i] - cp[i]) / dcp[i]
+  sigma[i] = (sigma[i] - cpH[i]) / dcpH[i]
 
-Cdp = [3 * R / mp[i] for i in range(len(mp))]
+Cdp = [3 * R for i in range(len(mp))]
 
-ms.ple("cp", cp, dcp)
-ms.ple("cpMol", cpMol, dcpMol)
+ms.ple("Tp", Tp, dTp)
+ms.ple("Tm", Tm, dTm)
+ms.ple("cpH", cpH, dcpH)
+ms.pl("sigma", sigma)
+ms.ple("cpMolH", cpMolH, dcpMolH)
 ms.pl("Cdp", Cdp)
-#ms.pl("sigma", sigma)
 
 # T=-196°C
 Tn = -195.8
 Qv = 199e3
+Tl = 23.1
+dTl = 0.4
+mp = [138.32e-3, 35.200e-3, 44.750e-3]
+dmp = [0.04e-3, 0.020e-3, 0.020e-3]
 mn0 = [505.9e-3, 484.1e-3, 455.60e-3]
 dmn0 = [0.3e-3, 0.3e-3, 0.10e-3]
 mn1 = [484.70e-3, 455.80e-3, 435.93e-3]
@@ -81,18 +83,28 @@ dmn1 = [0.05e-3, 0.05e-3, 0.05e-3]
 mn = [mn0[i] - mn1[i] for i in range(len(mn0))]
 dmn = [m.sqrt(dmn0[i]**2 + dmn1[i]**2) for i in range(len(mn0))]
 
-cp = []
-dcp = []
-cpMol = []
-dcpMol = []
+cpN = []
+dcpN = []
+cpMolN = []
+dcpMolN = []
 for i in range(len(mn)):
-  cp.append(Qv * mn[i] / (mp[i] * (Tl - Tn)))
-  cpMol.append(Mp[i] * cp[i])
+  cpN.append(Qv * mn[i] / (mp[i] * (Tl - Tn)))
+  dcpN.append(1.0 / (mp[i] * (Tl - Tn)) * m.sqrt(
+      (Qv * dmn[i])**2
+      + (Qv * mn[i])**2 * ((dmp[i] / mp[i])**2 + 1.0 / (Tl - Tn)**2 * dTl**2)))
+  cpMolN.append(Mp[i] * cpN[i])
+  dcpMolN.append(Mp[i] * dcpN[i])
 
 ms.ple("mn", mn, dmn)
 ms.ple("mp", mp, dmp)
-ms.pv("Tn", Tn)
-ms.pve("Tl", Tl, dTl)
-ms.pv("Qv", Qv)
-ms.pl("cp", cp)
-ms.pl("cpMol", cpMol)
+ms.ple("cpN", cpN, dcpN)
+ms.ple("cpMolN", cpMolN, dcpMolN)
+
+# Verhältnis
+f = []
+df = []
+for i in range(len(cpH)):
+  f.append(cpN[i] / cpH[i])
+  df.append(1 / cpH[i] * m.sqrt(dcpN[i]**2 + (cpN[i] / cpH[i] * dcpH[i])**2))
+
+ms.ple("cpN / cpH", f, df)
--- PAP1/measure.py
+++ PAP1/measure.py
@@ -1,5 +1,7 @@
 import math as m
 
+p = 0.001
+
 def mean_value(x):
   s = 0
   for i in range(len(x)):
@@ -18,7 +20,7 @@ def std_dev_m(x):
     qs += (x[i] - mean_value(x))**2
   return m.sqrt(qs / ((len(x) - 1) * len(x)))
 
-def reg_grad(x, y, dy):
+def reg_grad_y(x, y, dy):
   s0 = 0
   s1 = 0
   s2 = 0
@@ -33,7 +35,7 @@ def reg_grad(x, y, dy):
   eta = s0 * s3 - s1 * s1
   return (s0 * s4 - s1 * s2) / eta
 
-def reg_grad_err(x, y, dy):
+def reg_grad_err_y(x, y, dy):
   s0 = 0
   s1 = 0
   s2 = 0
@@ -47,6 +49,20 @@ def reg_grad_err(x, y, dy):
     s4 += x[i] * y[i] / (dy[i] * dy[i])
   eta = s0 * s3 - s1 * s1
   return m.sqrt(s0 / eta)
+
+def reg_grad(x, y, dx, dy):
+  g = reg_grad_y(x, y, dy)
+  print("g = " + str(g))
+  gOld = 2 * g
+  while abs(1 - gOld / g) > p:
+    gOld = g  
+    g = reg_grad_y(x, y, m.sqrt((g * dx)**2 + dy**2))
+    print("g = " + str(g))
+  return g
+
+def reg_grad_err(x, y, dx, dy):
+  g = reg_grad(x, y, dx, dy)
+  return reg_grad_err_y(x, y, m.sqrt((g * dx)**2 + dy**2))
 
 def sigerr(err):
   if ("{0:.1e}".format(err)[0] == "1" or "{0:.1e}".format(err)[0] == "2"):
