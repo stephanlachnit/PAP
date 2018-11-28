927ba55b54d24f99598cbf704dfc21ff4cd57b8d
Stephan Lachnit <stephanlachnit@users.noreply.github.com>
2018-11-28 15:46:00 +0100

    PAP21-213: use seconds insted of minutes for damping constant
    Signed-off-by: Stephan Lachnit <stephanlachnit@users.noreply.github.com>

--- PAP21-213.py
+++ PAP21-213.py
@@ -8,7 +8,7 @@ r_k = 50.8e-3
 
 damping_umin = np.array([1070, 970, 890, 808, 737, 675, 620, 569], dtype='float')
 damping_dumin = np.array([10, 5, 4, 2, 3, 3, 2, 4], dtype='float')
-damping_t = np.array([0, 2, 4, 6, 8, 10, 12, 14], dtype='float')
+damping_t = np.array([0, 2*60, 4*60, 6*60, 8*60, 10*60, 12*60, 14*60], dtype='float')
 
 moa_umin_1x15 = np.array([640, 560, 420, 300], dtype='float')
 moa_Tp_1x15 = np.array([104, 90, 70, 49], dtype='float')
@@ -42,6 +42,9 @@ dD = dslope
 f0 = exp(yitc)
 df0 = exp(yitc) * dyitc
 
+t_hv = ln(2.) / D
+t_hv_dsys = ln(2.) * dD / D**2
+
 dampingplot = plot(title='Linear regression of the damping constant', xlabel='t / s', ylabel='f / Hz', fig=1, scale='linlog')
 dampingplot.plt.grid(False)
 dampingplot.plotdata(t, f, df)
@@ -50,8 +53,9 @@ dampingplot.plotfunc(t, (f0+df0) * exp(-(D+dD) * t), label='line of uncertanty')
 
 print()
 print(val('damping constant', D, dD))
+print(val('half value time', t_hv, t_hv_dsys))
 
-# moment of anertia
+# moment of anertia Iz with precession
 f_m1x15 = moa_umin_1x15 / 120. * (1. + exp(-D * moa_Tp_1x15))
 df_m1x15 = 1. / 120. * sqrt((moa_dumin * (1. + exp(-D * moa_Tp_1x15)))**2 + (moa_umin_1x15 * exp(-D * moa_Tp_1x15))**2 * ((D * moa_dTp)**2 + (dD * moa_Tp_1x15)**2))
 f_m1x20 = moa_umin_1x20 / 120. * (1. + exp(-D * moa_Tp_1x20))
@@ -61,7 +65,7 @@ df_m2x15 = 1. / 120. * sqrt((moa_dumin * (1. + exp(-D * moa_Tp_2x15)))**2 + (moa
 f_m2x20 = moa_umin_2x20 / 120. * (1. + exp(-D * moa_Tp_2x20))
 df_m2x20 = 1. / 120. * sqrt((moa_dumin * (1. + exp(-D * moa_Tp_2x20)))**2 + (moa_umin_2x20 * exp(-D * moa_Tp_2x20))**2 * ((D * moa_dTp)**2 + (dD * moa_Tp_2x20)**2))
 
-moaplot = plot(title='precession period as function of the rotation frequency', xlabel='f / Hz', ylabel='Tp / s', fig=2)
+moaplot = plot(title='precession period as function of the rotation frequency', xlabel='f / Hz', ylabel=r'$T_p$ / s', fig=2)
 [slope_1x15, dslope_1x15, yitc_1x15, dyitc_1x15] = linreg(f_m1x15, moa_Tp_1x15, moa_dTp, df_m1x15, lrplot=moaplot, graphname='1@15')
 [slope_1x20, dslope_1x20, yitc_1x20, dyitc_1x20] = linreg(f_m1x20, moa_Tp_1x20, moa_dTp, df_m1x20, lrplot=moaplot, graphname='1@20')
 [slope_2x15, dslope_2x15, yitc_2x15, dyitc_2x15] = linreg(f_m2x15, moa_Tp_2x15, moa_dTp, df_m2x15, lrplot=moaplot, graphname='2@15')
@@ -97,7 +101,7 @@ print(sig('0-yitc 1@20', yitc_1x20, dyitc_1x20, 0.0))
 print(sig('0-yitc 2@15', yitc_2x15, dyitc_2x15, 0.0))
 print(sig('0-yitc 2@20', yitc_2x20, dyitc_2x20, 0.0))
 print()
-print(lst('Moment of anertia Iz', [Iz_1x15, Iz_1x20, Iz_2x15, Iz_2x20], [dIz_1x15, dIz_1x20, dIz_2x15, dIz_2x20]))
+print(lst('Moment of anertia Iz', Iz_list, dIz_list))
 print()
 print(val('Iz (mean value)', Iz, Iz_dtot))
 
@@ -107,7 +111,7 @@ dOmega = 2.*pi * 10. * cf_d10u / cf_10u**2
 wf = 2.*pi * cf_f
 dwf = 2.*pi * cf_df
 
-cfplot = plot(title=r'angular color speed Ω as function of the angular rotation speed $ω_f$', xlabel='ωf / rad/s', ylabel='Ω / rad/s', fig=4)
+cfplot = plot(title=r'angular color speed Ω as function of the angular rotation speed $ω_f$', xlabel=r'$ω_f$ / rad/s', ylabel='Ω / rad/s', fig=3)
 [slope, dslope, yitc, dyitc] = linreg(wf, Omega, dOmega, dwf, lrplot=cfplot)
 
 cf_Ix = Iz / (1 - slope)
@@ -116,11 +120,12 @@ cf_dIx = 1 / (1 - slope) * sqrt(Iz_dtot**2 + (Iz * dslope / (1 - slope))**2)
 print(val('Ix (color)     ', cf_Ix, cf_dIx))
 
 # nutation
-nutplot = plot(title = r'nutation frequency $f_n$ as a function of figure frequency $f_f$', xlabel='f_f / Hz', ylabel='f_n / Hz', fig=3)
 f_f = nut_umin_f / 60.
 df_f = nut_dumin_f / 60.
 f_n = nut_umin_n / 60.
 df_n = nut_dumin_n / 60.
+
+nutplot = plot(title = r'nutation frequency $f_n$ as a function of figure frequency $f_f$', xlabel=r'$f_f$ / Hz', ylabel=r'$f_n$ / Hz', fig=4)
 [slope, dslope, yitc, dyitc] = linreg(f_f, f_n, df_n, df_f, lrplot=nutplot)
 
 nut_Ix = Iz / slope
@@ -138,8 +143,9 @@ print(val('Theoretical Value', I_sphere))
 print(sig('dev Iz', Iz, Iz_dtot, I_sphere))
 print(sig('dev Ix (cf)', cf_Ix, cf_dIx, I_sphere))
 print(sig('dev Ix (nut)', nut_Ix, nut_dIx, I_sphere))
+print(val('Iz / Ix (cf)', Iz / cf_Ix))
+print(val('Iz / Ix (nut)', Iz / nut_Ix))
 
 # show matplotlib figs
 print()
 plot.showfigs()
-
