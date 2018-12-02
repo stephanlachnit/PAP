75b1aeba67b0de321b723ccd8f7427ba7d59680a
Stephan Lachnit <stephanlachnit@users.noreply.github.com>
2018-12-02 16:33:42 +0100

    PAP21-222: updated measured values
    Signed-off-by: Stephan Lachnit <stephanlachnit@users.noreply.github.com>

--- PAP21-222.py
+++ PAP21-222.py
@@ -1,31 +1,33 @@
 # measure version 1.8
-from measure import sqrt,val
+from measure import sqrt,val,npfarray,mv,dsto_mv
 
 # values
 cw = 4180
 rohw = 998 # https://www.internetchemie.info/chemie-lexikon/daten/w/wasser-dichtetabelle.php
 rohw_dsys = 1
 
-km_U_mot = 1.
-km_U_mot_dsys = 1.
-km_I_mot = 1.
-km_I_mot_dsys = 1.
-km_U_heiz = 1.
-km_U_heiz_dsys = 1.
-km_I_heiz = 1.
-km_I_heiz_dsys = 1.
-km_f = 1.
-km_f_dsys = 1.
+U_mot = 24.0
+U_mot_dsys = 0.1
+I_mot = 2.3
+I_mot_dsys = 0.1
+
+km_U_heiz = 5.58
+km_U_heiz_dsys = 0.04
+km_I_heiz = 5.7
+km_I_heiz_dsys = 0.1
+km_f = 325.2 / 60.
+km_f_dsys = 0.1 / 60.
 km_t = 180.
 km_t_dsys = 15.
 km_dT = 22.1 - 18.65
 km_dT_dsys = sqrt(0.25**2 + 0.1**2)
-km_Vps = 1.
-km_Vps_dsys = 1.
+Vpm = npfarray([199.5,198.4,200.7,200.1,200.5])
+km_Vps = mv(Vpm / 6e5)
+km_Vps_dsys = dsto_mv(Vpm / 6e5)
 
 # Kältemaschine
-Wm = km_U_mot * km_I_mot / km_f
-Wm_dsys = 1./km_f * sqrt((km_U_mot * km_I_mot_dsys)**2 + (km_U_mot_dsys * km_I_mot)**2 + (km_U_mot * km_I_mot * km_f_dsys / km_f)**2)
+Wm = U_mot * I_mot / km_f
+Wm_dsys = 1./km_f * sqrt((U_mot * I_mot_dsys)**2 + (U_mot_dsys * I_mot)**2 + (U_mot * I_mot * km_f_dsys / km_f)**2)
 Wh = km_U_heiz * km_I_heiz / km_f
 Wh_dsys = 1./km_f * sqrt((km_U_heiz * km_I_heiz_dsys)**2 + (km_U_heiz_dsys * km_I_heiz)**2 + (km_U_heiz * km_I_heiz * km_f_dsys / km_f)**2)
 
@@ -37,7 +39,7 @@ tWges_dsys = cw / km_f * sqrt((rohw_dsys * km_dT * km_Vps)**2 + (rohw * km_dT_ds
 eWges = km_t  * km_f * (Wm + Wh)
 eWges_dsys = sqrt((km_t_dsys  * km_f * (Wm + Wh))**2 + (km_t  * km_f_dsys * (Wm + Wh))**2 + (km_t  * km_f * (Wm_dsys + Wh_dsys))**2)
 
-dW = tWges - eWges
+dW = eWges - tWges
 dW_dsys = sqrt(tWges_dsys**2 + eWges_dsys**2)
 
 print()
