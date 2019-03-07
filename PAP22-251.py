09c1bca5024661ff23a6d098548d4c01c46c626f
Stephan Lachnit <stephanlachnit@protonmail.com>
2019-03-07 21:04:08 +0100

    PAP22-251: update
    Signed-off-by: Stephan Lachnit <stephanlachnit@protonmail.com>

--- PAP22-251.py
+++ PAP22-251.py
@@ -10,10 +10,13 @@ N_dsto = sqrt(N)
 
 pltext.initplot(num=1,title='Abbildung   : Zählrohrcharakteristik',xlabel='Spannung in V',ylabel='Ereignisse')
 pltext.plotdata(U,N,N_dsto,label='Messwerte')
-linreg(U[1:],N[1:],N_dsto[1:],plot=True,prange=(410,580))
+[slope,dslope] = linreg(U[1:],N[1:],N_dsto[1:],plot=True,prange=(410,580))[0:2]
 pltext.set_layout(xlim=(410,580),ylim=(1800,2500))
 
 U0 = 510
+
+print('\nAufgabe 2\n')
+print(val(slope,dslope,name='Steigung'))
 
 # Aufgabe 3
 U = [510,610]
@@ -46,22 +49,23 @@ k_delta = k100 - k0
 k_delta_err = sqrt(k100_err**2 + k0_err**2)
 
 t_array = nplinspace(50,190)
-pltext.initplot(num=2,title='Abbildung   : Zählrate als Funktion der Zeit',xlabel='Zeit in s',ylabel='Ereignisse')
+pltext.initplot(num=2,title='Abbildung   : Ereignisse als Funktion der Zeit',xlabel='Zeit in s',ylabel='Ereignisse')
 pltext.plotdata([60,180],[N1min[0],N3min[0]],[N1min_dsto[0],N3min_dsto[0]],label=r'Messwerte $U_0+0V$')
 pltext.plotdata([60,180],[N1min[1],N3min[1]],[N1min_dsto[1],N3min_dsto[1]],label=r'Messwerte $U_0+100V$')
 plt.plot(t_array,fitfunc_3(t_array,*p_opt_0),label=r'Fit $U_0 +0V$')
 plt.plot(t_array,fitfunc_3(t_array,*p_opt_100),label=r'Fit $U_0 +100V$')
 pltext.set_layout(xlim=(50,190),ylim=(0.9e4,3.1e4))
 
-print()
+print('\nAufgabe 3\n')
 print(val(anstieg_1min,anstieg_1min_dsto,name='abs Anstieg (1min)',percerr=True))
 print(val(rel_anstieg_1min,rel_anstieg_1min_dsto,name='rel Anstieg (1min)',percerr=True))
 print(val(anstieg_3min,anstieg_3min_dsto,name='abs Anstieg (3min)',percerr=True))
 print(val(rel_anstieg_3min,rel_anstieg_3min_dsto,name='rel Anstieg (3min)',percerr=True))
-print(dev(anstieg_1min,anstieg_1min_dsto,anstieg_3min,anstieg_3min_dsto,'Abw rel Anstiege'))
+print(dev(anstieg_1min,anstieg_1min_dsto,anstieg_3min,anstieg_3min_dsto,'Abw abs Anstiege'))
+print(dev(rel_anstieg_1min,rel_anstieg_1min_dsto,rel_anstieg_3min,rel_anstieg_3min_dsto,'Abw rel Anstiege'))
 print(val(k0,k0_err,name='Zählrate U0+0V',percerr=True))
 print(val(k100,k100_err,name='Zählrate U0+100V',percerr=True))
-print(val(k_delta,k_delta_err,name='Zählrate +100V',percerr=True))
+print(val(k_delta,k_delta_err,name='Zählrate U+100V',percerr=True))
 print(val(t_1perc,name='t (1%)'))
 
 # Aufgabe 4
@@ -77,16 +81,28 @@ def poisson(x,A,mu):
 p_opt_g,p_err_g = spcurvefit(gauss,anzahl[5:-12],ereig[5:-12],yerr=fehler[5:-12],p0=[2300,70,8])
 p_opt_p,p_err_p = spcurvefit(poisson,anzahl[5:-12],ereig[5:-12],yerr=fehler[5:-12],p0=[2300,70])
 
-x_array = nplinspace(40,100)
+from numpy import sum
+from scipy.stats import chi2
+chi2_g = sum((gauss(anzahl[5:-12],*p_opt_g) - ereig[5:-12])**2 / fehler[5:-12]**2)
+dof_g = len(anzahl[5:-12]) - 3
+chi2_red_g = chi2_g / dof_g
+chi2_p = sum((poisson(anzahl[5:-12],*p_opt_p) - ereig[5:-12])**2 / fehler[5:-12]**2)
+dof_p = len(anzahl[5:-12]) - 2
+chi2_red_p = chi2_p / dof_p
+prob_g = 1 - chi2.cdf(chi2_g,dof_g)
+prob_p = 1 - chi2.cdf(chi2_p,dof_p)
 
+x_array = nplinspace(40,100)
 pltext.initplot(num=3,title='Abbildung   : Statistik des radioaktiven Zerfalls',xlabel='Zerfälle in 1/s',ylabel='Häufigkeit')
-pltext.plotdata(anzahl,ereig,fehler,label='Messwerte',caps=True)
+pltext.plotdata(anzahl,ereig,fehler,label='Messwerte')
 plt.plot(x_array,gauss(x_array,*p_opt_g),label='Gauss Fit')
 plt.plot(x_array,poisson(x_array,*p_opt_p),label='Poisson Fit')
 pltext.set_layout(xlim=(40,100),ylim=(-2,142))
 
 print('\nAufgabe 4:\n')
 print(tbl([['Fitmethode','A','mu','sig'],['Gauss',val(p_opt_g[0],p_err_g[0]),val(p_opt_g[1],p_err_g[1]),val(p_opt_g[2],p_err_g[2])],['Poisson',val(p_opt_p[0],p_err_p[0]),val(p_opt_p[1],p_err_p[1]),'']]))
+print()
+print(tbl([['Fitmethode','Chi2','Chi2red','Fitwkeit'],['Gauss',val(chi2_g),val(chi2_red_g),val(prob_g)],['Poisson',val(chi2_p),val(chi2_red_p),val(prob_p)]]))
 
 # Aufgabe 5
 anzahl,ereig = loadtxt('data/251_A5.dat',unpack=True)
@@ -95,16 +111,26 @@ fehler = sqrt(ereig)
 p_opt_g,p_err_g = spcurvefit(gauss,anzahl[0:-2],ereig[0:-2],yerr=fehler[0:-2],p0=[5200,4,2])
 p_opt_p,p_err_p = spcurvefit(poisson,anzahl[0:-2],ereig[0:-2],yerr=fehler[0:-2],p0=[5200,4])
 
-x_array = nplinspace(0,14)
+chi2_g = sum((gauss(anzahl[0:-2],*p_opt_g) - ereig[0:-2])**2 / fehler[0:-2]**2)
+dof_g = len(anzahl[0:-2]) - 3
+chi2_red_g = chi2_g / dof_g
+chi2_p = sum((poisson(anzahl[0:-2],*p_opt_p) - ereig[0:-2])**2 / fehler[0:-2]**2)
+dof_p = len(anzahl[0:-2]) - 2
+chi2_red_p = chi2_p / dof_p
+prob_g = 1 - chi2.cdf(chi2_g,dof_g)
+prob_p = 1 - chi2.cdf(chi2_p,dof_p)
 
+x_array = nplinspace(0,14)
 pltext.initplot(num=4,title='Abbildung   : Statistik des radioaktiven Zerfalls',xlabel='Zerfälle in 1/s',ylabel='Häufigkeit',scale='linlog')
-pltext.plotdata(anzahl,ereig,fehler,label='Messwerte',caps=True)
+pltext.plotdata(anzahl,ereig,fehler,label='Messwerte')
 plt.plot(x_array,gauss(x_array,*p_opt_g),label='Gauss Fit')
 plt.plot(x_array,poisson(x_array,*p_opt_p),label='Poisson Fit')
 pltext.set_layout(xlim=(0,14),ylim=(4e-1,2e3))
 
 print('\nAufgabe 5:\n')
 print(tbl([['Fitmethode','A','mu','sig'],['Gauss',val(p_opt_g[0],p_err_g[0]),val(p_opt_g[1],p_err_g[1]),val(p_opt_g[2],p_err_g[2])],['Poisson',val(p_opt_p[0],p_err_p[0]),val(p_opt_p[1],p_err_p[1]),'']]))
+print()
+print(tbl([['Fitmethode','Chi2','Chi2red','Fitwkeit'],['Gauss',val(chi2_g),val(chi2_red_g),val(prob_g)],['Poisson',val(chi2_p),val(chi2_red_p),val(prob_p)]]))
 
 # Plot
 print()
